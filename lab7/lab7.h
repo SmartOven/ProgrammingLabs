a3b28d5ff02dfa2699f48e187a819468ba706685
SmartOven <yarik.panteleev@gmail.com>
2020-05-19 16:25:05 +0300

    updated 19.05

--- lab7/lab7.h
+++ lab7/lab7.h
@@ -9,27 +9,32 @@ public:
     class iterator : public std::iterator<std::random_access_iterator_tag, T> {
     public:
     	// Хрень для stl алгоритмов
-        using difference_type = std::iterator<std::random_access_iterator_tag, T>::difference_type;
+        using difference_type = typename std::iterator<std::random_access_iterator_tag, T>::difference_type;
         difference_type operator- (const iterator& other) const { return _data - other._data; }
     	// end Хрень
     	
-        iterator(T*); // Скрытый конструктор для создания итератора в классе буффера
-        iterator(const iterator& src); // Конструктор для неявного копирования
+        explicit iterator(T* start) {
+            _data = start;
+        } // Скрытый конструктор для создания итератора в классе буффера
+        iterator(const iterator& src) { _data = src._data; } // Конструктор для неявного копирования
     	~iterator() = default; // Дефолтный деструктор
-        T& operator*(); // Разыменовывание указателя
-        T* operator->(); // Получение данных по указателю
+        T& operator*() { return *(_data); } // Разыменовывание указателя
+        T* operator->() { return _data; } // Получение данных по указателю
         
         // Операторы сложения и вычитания
-        iterator operator++();
-        iterator operator++(int);
-        T* operator--();
-        iterator operator+(int);
-        iterator operator-(int) const;
+        iterator operator++() {++_data; return *this;}
+        iterator operator--() {--_data; return *this;}
+        iterator operator+(int x) {_data = _data + x; return *this;}
+        iterator operator-(int x) {_data = _data - x; return *this;}
         // end Операторы сложения и вычитания
 
     	// Операторы сравнения
-        bool operator==(const iterator&) const;
-        bool operator!=(const iterator&) const;
+        bool operator==(const iterator& other) const {return _data == other._data;}
+        bool operator!=(const iterator& other) const {return _data != other._data;}
+        bool operator<(const iterator& other) const {return _data < other._data;}
+        bool operator<=(const iterator& other) const {return _data <= other._data;}
+        bool operator>(const iterator& other) const {return _data > other._data;}
+        bool operator>=(const iterator& other) const {return _data >= other._data;}
         // end Операторы сравнения
     private:
         T* _data = nullptr;
@@ -40,27 +45,27 @@ public:
         return iterator(data);
     }
     iterator end() const {
-        return iterator(data + _size + 1);
+        return iterator(data + _size);
     }
     // Получение капасити и размера массива
-    size_t capacity() const {
+    [[nodiscard]] size_t capacity() const {
 	    return this->_capacity;
     }
-    size_t size() const {
+    [[nodiscard]] size_t size() const {
 	    return this->_size;
     }
 	
     // Конструкторы класса
-    explicit CircleBuffer(size_t capacity) :
-        _capacity(capacity),
-        data(new T[_capacity]) {
+    explicit CircleBuffer(size_t capacity) {
+        _capacity = capacity;
+        data = new T[_capacity];
     }
-    CircleBuffer(std::initializer_list<T> list) :
-        _capacity(list.size()),
-        data(new T[list.size()]) {
+    /*explicit CircleBuffer(std::initializer_list<T> list) {
+        _capacity = list.size();
+        data = new T[list.size()];
         for (auto it = list.begin(); it != list.end(); ++it)
             push_back(*it);
-    }
+    }*/
     CircleBuffer(const CircleBuffer& other) :
 		_size(other._size),
         _capacity(other._capacity),
@@ -72,13 +77,26 @@ public:
     }
 	
 	// Методы класса
-    void push_front(T x)
-    {
-        
+    void push_front(T x) {
+        if (_size < _capacity) {
+            for (size_t i = _size; i >= 1; i--) {
+                data[i] = data[i - 1];
+            }
+            data[0] = x;
+            _size++;
+        }
+        else {
+            for (size_t i = _capacity - 1; i >= 1; i--) {
+                data[i] = data[i - 1];
+            }
+            data[0] = x;
+        }
     }
-    void pop_front()
-    {
-
+    void pop_front() {
+        for (size_t i = 0; i < _size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        _size--;
     }
     void push_back(T x) {
         if (_size < _capacity) {
@@ -92,26 +110,45 @@ public:
             data[_capacity - 1] = x;
         }
     }
-    void pop_back()
-	{
-
+    void pop_back() {
+        if (_size > 0) {
+            _size--;
+        }
     }
-	void insert(size_t ind)
-    {
-	    
+	void insert(size_t ind, T x) {
+        data[ind % _capacity] = x;
     }
-	void remove(size_t ind)
-    {
-	    
+    void insert(iterator ind, T x) {
+        *(begin() + (ind - begin()) % _size) = x;
     }
-	void resize(size_t new_capacity)
-    {
-	    
+	void remove(size_t ind) {
+        for (size_t i = ind + 1; i < _size; i++) {
+            data[(i - 1) % _capacity] = data[i % _capacity];
+        }
+        _size--;
+    }
+	void resize(size_t new_capacity) {
+	    T* new_data = new T[new_capacity];
+	    _size = std::min(_size, new_capacity);
+	    for (size_t i = 0; i < _size; i++) {
+	        new_data[i] = data[i];
+	    }
+	    delete data;
+	    data = new_data;
+	    _capacity = new_capacity;
     }
 
 	// Операторы класса
 	T operator[] (size_t ind) {
         return data[ind % _capacity];
+    }
+
+    CircleBuffer& operator=(std::initializer_list<T> list) {
+        _capacity = list.size();
+        data = new T[list.size()];
+        for (auto it = list.begin(); it != list.end(); ++it)
+            push_back(*it);
+        return *this;
     }
 private:
     size_t _size = 0;
--- lab7/lab7_main.cpp
+++ lab7/lab7_main.cpp
@@ -1,13 +1,73 @@
 #include <iostream>
+#include <algorithm>
 #include "lab7.h"
 using std::cin;
 using std::cout;
 
+template <class T>
+void print_info(const CircleBuffer<T>& v) {
+    static int COUNT = 1;
+    cout << "Op. number: " << COUNT << "\n";
+    cout << "Capacity: " << v.capacity() << "\n";
+    cout << "Size: " << v.size() << "\n";
+    cout << "Items: ";
+    for (auto it : v) {
+        cout << it << ' ';
+    }
+    cout << "\n";
+    cout << "\n";
+    COUNT++;
+}
+
 int main() {
-	CircleBuffer <int> v = { 1, 2, 3, 4 };
-	CircleBuffer <int>::iterator it = v.begin();
-	for (; it != v.end(); ++it) {
-		cout << *it << ' ';
-	}
+	CircleBuffer <int> v(3);
+	v.push_back(1);
+	v.push_back(3);
+    print_info(v); // 1
+
+    v.push_back(2);
+    print_info(v); // 2
+
+	v.push_back(5);
+    print_info(v); // 3
+
+    v.resize(5);
+    print_info(v); // 4
+
+    v.insert(1, -1);
+    print_info(v); // 5
+
+    v.push_back(-2);
+    print_info(v); // 6
+
+    v.remove(2);
+    print_info(v); // 7
+
+    v.resize(2);
+    print_info(v); // 8
+
+    v.pop_back();
+    print_info(v); // 9
+
+    v.push_front(2);
+    print_info(v); // 10
+
+    v.push_front(1);
+    print_info(v); // 11
+
+    v.pop_front();
+    print_info(v); // 12
+
+    v.push_front(3);
+    print_info(v); // 13
+
+    v.insert(v.begin(), -10);
+    print_info(v); // 14
+
+    v = {1, 2, 4, 5, 3};
+    print_info(v); // 15
+
+    std::sort(v.begin(), v.end());
+    print_info(v); // 16
     return 0;
 }
